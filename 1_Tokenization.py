5099661b4d903ac709547104cd33756be90e29a5
Kundan_Dewangan <kd8donati@gmail.com>
2019-03-02 12:10:54 +0530

    Words and Sentence tokenization via NLTK

--- 1_Tokenization.py
+++ 1_Tokenization.py
@@ -1,5 +1,7 @@
 import nltk
 from nltk import sent_tokenize, word_tokenize
 text="Hi Mr. KD. This is a demo. Get to wrk, dont loose hope."
+#tokenize on the basis of sentences
 print (sent_tokenize(text)) 
+#tokenize on the basis of words
 print (word_tokenize(text))
